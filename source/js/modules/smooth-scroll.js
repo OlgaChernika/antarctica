62f85a667b452670fac88a1926fb2c6f9a58b0ec
Olga Chernikova <darkred2694@gmail.com>
2022-03-17 10:16:14 +0300

    Исправляет баги

--- source/js/modules/smooth-scroll.js
+++ source/js/modules/smooth-scroll.js
@@ -3,9 +3,7 @@ const anchors = document.querySelectorAll('[data-anchor]');
 const moveTo = new window.MoveTo();
 
 export const initAnchors = () => {
-  if (anchors.length > 0) {
-    anchors.forEach((anchor) => {
-      moveTo.registerTrigger(anchor);
-    });
-  }
+  anchors.forEach((anchor) => {
+    moveTo.registerTrigger(anchor);
+  });
 };
